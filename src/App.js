ff2969d39778c992413d093e5da290b10b058cde
joshydotpoo <j.freilichd@gmail.com>
2018-10-31 00:13:20 -0700

    made a flexbox example for responsive cards

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
+import MetaTags from 'react-meta-tags';
 import logo from './logo.svg';
 import './App.css';
+import './css/recipe_card.css'
 
 class App extends Component {
-  componentWillMount() {
-    document.title = "Munchy";
-  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <MetaTags>
+          <title>Munchy</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        </MetaTags>
+        <div id="recipe_card_container">
+          <div className='recipe_card'><img src="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?cs=srgb&dl=food-dinner-lunch-70497.jpg&fm=jpg"/></div>
+          <div className='recipe_card'><img src="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?cs=srgb&dl=food-dinner-lunch-70497.jpg&fm=jpg"/></div>
+          <div className='recipe_card'><img src="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?cs=srgb&dl=food-dinner-lunch-70497.jpg&fm=jpg"/></div>
+          <div className='recipe_card'><img src="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?cs=srgb&dl=food-dinner-lunch-70497.jpg&fm=jpg"/></div>
+          <div className='recipe_card'><img src="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?cs=srgb&dl=food-dinner-lunch-70497.jpg&fm=jpg"/></div>
+          <div className='recipe_card'><img src="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?cs=srgb&dl=food-dinner-lunch-70497.jpg&fm=jpg"/></div>
+          <div className='recipe_card'><img src="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?cs=srgb&dl=food-dinner-lunch-70497.jpg&fm=jpg"/></div>
+          <div className='recipe_card'><img src="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?cs=srgb&dl=food-dinner-lunch-70497.jpg&fm=jpg"/></div>
+        </div>
       </div>
     );
   }
